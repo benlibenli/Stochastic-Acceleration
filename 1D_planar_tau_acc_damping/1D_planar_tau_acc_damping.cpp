640cc5748601e36b97384ecb969d84b8136859f4
Ben Li <ben.li@gssi.it>
2025-09-01 15:34:43 +0200

    updated diffusion coefficient

--- 1D_planar_tau_acc_damping/1D_planar_tau_acc_damping.cpp
+++ 1D_planar_tau_acc_damping/1D_planar_tau_acc_damping.cpp
@@ -55,7 +55,7 @@ int main(){
     cout << "Alven speed: " << vA << endl;
     cout << "velocity ratio: " << u2/vA << ", " << u2/Sqrt_Delta << endl;
 
-    // create mesh grid in momentum space
+    // create meshgrid in momentum space
 
     int nj = 7001;
     int i, j, k;
@@ -66,21 +66,21 @@ int main(){
 
     // calculate the diffusion coefficient
 
-    double Dz[nj];
+    double Dz_i[nj];
     double rL, kres, Wp;
     for(j=0; j<nj; ++j){
         rL = p[j]*c/e/B;
         kres = 2.0*pi/rL;
         Wp = pow(kres,-5.0/3.0)*pow(sqrt(rho)*S/a,2.0/3.0);
-        Dz[j] = 1.0/3.0*c*rL/(kres*Wp/(B*B/4.0/pi));
+        Dz_i[j] = 1.0/3.0*c*rL/(kres*Wp/(B*B/4.0/pi));
     }
 
-    cout << "diffusion coefficient: " << Dz[0] << ", " << Dz[3000] << ", " << Dz[6000] << endl;
+    cout << "diffusion coefficient: " << Dz_i[0] << ", " << Dz_i[3000] << ", " << Dz_i[6000] << endl;
 
     // timescale ratio at 1 PeV
 
-    double tau_sa = 9.0*Dz[6000]/pow(vA,2.0);
-    double tau_esc = pow(z0,2.0)/Dz[6000];
+    double tau_sa = 9.0*Dz_i[6000]/pow(vA,2.0);
+    double tau_esc = pow(z0,2.0)/Dz_i[6000];
 
     cout << "timescale ratio: " << tau_sa/tau_esc << endl;
 
@@ -93,8 +93,8 @@ int main(){
         Int_1 = 0.0;
         Int_2 = 0.0;
         for(i=0; i<j; ++i){
-            Int_1 += -3.0*u2/(u2-u1)/(1.0-exp(u2*z0/Dz[i]))/p[i]*(p[i+1]-p[i]);
-            Int_2 += -(3.0/2.0*u2/(u2-u1)+3.0*Sqrt_Delta/(u2-u1)*(1.0/(1.0-exp(Sqrt_Delta*z0/Dz[i]))-1.0/2.0))/p[i]*(p[i+1]-p[i]);
+            Int_1 += -3.0*u2/(u2-u1)/(1.0-exp(u2*z0/Dz_i[i]))/p[i]*(p[i+1]-p[i]);
+            Int_2 += -(3.0/2.0*u2/(u2-u1)+3.0*Sqrt_Delta/(u2-u1)*(1.0/(1.0-exp(Sqrt_Delta*z0/Dz_i[i]))-1.0/2.0))/p[i]*(p[i+1]-p[i]);
         }
         f0_1[j] = pow(p[j],3.0*u1/(u2-u1))*exp(Int_1);
         f0_2[j] = pow(p[j],3.0*u1/(u2-u1))*exp(Int_2);
@@ -102,8 +102,8 @@ int main(){
 
     // calculate normalization
  
-    double Pram = rho*u1*u1;
-    // double Pram = Mdot*u1/(4.0*pi*pow(15.0*pc,2.0));
+    // double Pram = rho*u1*u1;
+    double Pram = Mdot*u1/(4.0*pi*pow(15.0*pc,2.0));
     double v[nj]; 
     for(j=0; j<nj; ++j){
         v[j] = p[j]*c/sqrt(pow(mp*c,2.0)+pow(p[j],2.0));
@@ -124,14 +124,30 @@ int main(){
         f0_2[j] = norm_2 * f0_2[j];
     }
 
+    // calculate spectrum away from the shock
+
+    double z = 0.5*z0;
+
+    double B1[nj], B2[nj];
+    for(j=0; j<nj; ++j){
+        B1[j] = (u2+Sqrt_Delta)/2/Dz_i[j];
+        B2[j] = (u2-Sqrt_Delta)/2/Dz_i[j];
+    }
+
+    double f_1[nj], f_2[nj];
+    for(j=0; j<nj; ++j){
+        f_1[j] = f0_1[j]/(1.0-exp(-u2*z0/Dz_i[j]))*(1.0-exp(u2*(z-z0)/Dz_i[j]));
+        f_2[j] = f0_2[j]/(1.0-exp((B1[j]-B2[j])*z0))*(exp(B1[j]*z)-exp((B1[j]-B2[j])*z0)*exp(B2[j]*z));
+    }
+
     // update the diffusion coefficient
 
-    double Gamma, Int_3, dk, kp;
-    #pragma omp parallel for private(i, k, rL, kres, dk, kp, Gamma, Int_3, Wp) shared(Dz, f0_2, p) num_threads(30)
+    double Dz_f[nj], Int_3[nj], Gamma, dk, kp;
+    #pragma omp parallel for private(i, k, rL, kres, dk, kp, Gamma, Wp) shared(Dz_f, f0_2, p) num_threads(30)
     for(j=0; j<nj; ++j){
         rL = p[j]*c/e/B;
         kres = 2.0*pi/rL;
-        Int_3 = 0.0;
+        Int_3[j] = 0.0;
         if(kres>k0){
             dk = (kres-k0)/1000.0;
             kp = k0;
@@ -143,24 +159,34 @@ int main(){
                     }
                 }
                 Gamma = 8.0*pow(pi,3.0)*pow(e,2.0)*pow(vA/c,2.0)/kp * Gamma;
-                Int_3 += Gamma/pow(kp,5.0/3.0)*dk;
+                Int_3[j] += Gamma/pow(kp,5.0/3.0)*dk;
                 kp = kp + dk;
             }
-            Int_3 = 1.0/3.0*pow(rho/S/a/a,1.0/3.0) * Int_3;
+            Int_3[j] = 1.0/3.0*pow(rho/S/a/a,1.0/3.0) * Int_3[j];
         }
-        // cout << Int_3 << endl;
-        // cout << kres << ", " << k0 << ", " << dk << endl;
-        Wp = pow(kres,-5.0/3.0)*pow(sqrt(rho)*S/a,2.0/3.0) * pow(1.0-Int_3,2.0);
-        Dz[j] = 1.0/3.0*c*rL/(kres*Wp/(B*B/4.0/pi));
+        Wp = pow(kres,-5.0/3.0)*pow(sqrt(rho)*S/a,2.0/3.0) * pow(1.0-Int_3[j],2.0);
+        Dz_f[j] = 1.0/3.0*c*rL/(kres*Wp/(B*B/4.0/pi));
     }
 
-    cout << "updated diffusion coefficient: " << Dz[0] << ", " << Dz[3000] << ", " << Dz[6000] << endl;
+    cout << "updated diffusion coefficient: " << Dz_f[0] << ", " << Dz_f[3000] << ", " << Dz_f[6000] << endl;
+
+    // output to file
 
     ofstream fout;
 
     fout.open("p.csv");
     for(j=0; j<nj; ++j)
         fout << p[j] << endl;
+    fout.close();
+
+    fout.open("Dz_i.csv");
+    for(j=0; j<nj; ++j)
+        fout << Dz_i[j] << endl;
+    fout.close();
+
+    fout.open("Dz_f.csv");
+    for(j=0; j<nj; ++j)
+        fout << Dz_f[j] << endl;
     fout.close();
 
     fout.open("f0_1.csv");
@@ -171,6 +197,16 @@ int main(){
     fout.open("f0_2.csv");
     for(j=0; j<nj; ++j)
         fout << f0_2[j] << endl;
+    fout.close();
+
+    fout.open("f_1.csv");
+    for(j=0; j<nj; ++j)
+        fout << f_1[j] << endl;
+    fout.close();
+
+    fout.open("f_2.csv");
+    for(j=0; j<nj; ++j)
+        fout << f_2[j] << endl;
     fout.close();
 
 }
